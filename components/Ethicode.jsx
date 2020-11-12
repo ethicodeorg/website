23c5c92b0ba75b916bd6f92c8766f89dd5758f82
scott harvey <donzail@hotmail.com>
2020-11-12 11:19:55 -0500

    Work in Progress - Implement the page in Next.js - Website Framework
    This commit should launch the website with some style and section headings.
    
    index.js - Replaced icon and added code that was in the Header section of https://github.com/atlisg/resume/blob/master/index.html
    
    components - added initial versions of the About, Contact, Ethicode, Header, and Work components.
    
    _app.js - set up structure for application by creating a container div "app" with the "frontground" classs and included the Header as well as another container div "page-container" that displays the components.

---
+++ components/Ethicode.jsx
@@ -0,0 +1,46 @@
+const Ethicode = () => {
+
+    return (
+		<section id="ethicode">
+			<h2 class="main-title">Ethicode</h2>
+
+			<style>{`
+				.main-title {
+					max-width: 1200px;
+					padding-top: 100px;
+					margin: 0 auto;
+					font-size: 36px;
+					text-align: left;
+					line-height: 50px;
+					color: #00bad4;
+					-webkit-animation: fadeInBottom 1s;
+					animation: fadeInBottom 1s;
+				}
+
+				@media screen and (min-width: 600px) {
+					.main-title {
+						padding-top: 160px;
+						font-size: 48px;
+						line-height: 100px;
+					}
+				}
+
+				@media screen and (min-width: 1000px) {
+					.main-title {
+						font-size: 72px;
+						line-height: 120px;
+					}
+				}
+
+				@media screen and (min-width: 1200px) {
+					.main-title {
+						font-size: 104px;
+						line-height: 120px;
+					}
+				}
+            `}</style>
+		</section>
+	);
+}
+
+export default Ethicode
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+          <div id="app" className="frontground">
+            <Header />
+            <div className="page-container">
+              <Component {...pageProps} />
+            </div>
+          </div>
+        )
 }
 
 export default MyApp
