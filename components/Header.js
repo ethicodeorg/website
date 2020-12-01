5437ef078a34ed2efb97e6d9cc598cab15f37546
scott harvey <donzail@hotmail.com>
2020-12-01 10:22:12 -0500

    Header Highlighting Chosen Page
    Using state to control which menu is shown and what classes are given to each link

--- components/Header.js
+++ components/Header.js
@@ -4,56 +4,120 @@ import Link from 'next/link';
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      menuType: 'dark',
-    };
+
+    ~props.currentPage.indexOf('About') &&
+      (this.state = {
+        menuType: 'dark',
+        ethicodeClass: 'menu-item menu-item-main menu-item-first',
+        aboutClass: 'menu-item menu-item-main menu-item-active',
+        workClass: 'menu-item menu-item-main',
+        contactClass: 'menu-item menu-item-main',
+      })
+
+    ~props.currentPage.indexOf('Contact') &&
+      (this.state = {
+        menuType: 'light',
+        ethicodeClass: 'menu-item menu-item-main menu-item-first',
+        aboutClass: 'menu-item menu-item-main menu-item-main-light',
+        workClass: 'menu-item menu-item-main menu-item-main-light',
+        contactClass: 'menu-item menu-item-main menu-item-main-light menu-item-active',
+      })
+
+    ~props.currentPage.indexOf('Work') &&
+      (this.state = {
+        menuType: 'light',
+        ethicodeClass: 'menu-item menu-item-main menu-item-first',
+        aboutClass: 'menu-item menu-item-main menu-item-main-light',
+        workClass: 'menu-item menu-item-main menu-item-main-light menu-item-active',
+        contactClass: 'menu-item menu-item-main menu-item-main-light',
+      })
+
+    ~props.currentPage.indexOf('Index') &&
+      (this.state = {
+        menuType: 'dark',
+        ethicodeClass: 'menu-item menu-item-main menu-item-first menu-item-active',
+        aboutClass: 'menu-item menu-item-main',
+        workClass: 'menu-item menu-item-main',
+        contactClass: 'menu-item menu-item-main',
+      });
   }
+
   componentDidUpdate(prevProps) {
     if (this.props.currentPage !== prevProps.currentPage) {
-      this.props.currentPage.indexOf('Work') >= 0 || this.props.currentPage.indexOf('Contact') >= 0
-        ? this.setState({ menuType: 'light' })
-        : this.setState({ menuType: 'dark' });
+      ~this.props.currentPage.indexOf('About') &&
+        (this.setState({
+          menuType: 'dark',
+          ethicodeClass: 'menu-item menu-item-main menu-item-first',
+          aboutClass: 'menu-item menu-item-main menu-item-active',
+          workClass: 'menu-item menu-item-main',
+          contactClass: 'menu-item menu-item-main',
+        }));
+
+      ~this.props.currentPage.indexOf('Contact') &&
+        (this.setState({
+          menuType: 'light',
+          ethicodeClass: 'menu-item menu-item-main menu-item-first',
+          aboutClass: 'menu-item menu-item-main menu-item-main-light',
+          workClass: 'menu-item menu-item-main menu-item-main-light',
+          contactClass: 'menu-item menu-item-main menu-item-main-light menu-item-active',
+        }));
+
+      ~this.props.currentPage.indexOf('Work') &&
+        (this.setState({
+          menuType: 'light',
+          ethicodeClass: 'menu-item menu-item-main menu-item-first',
+          aboutClass: 'menu-item menu-item-main menu-item-main-light',
+          workClass: 'menu-item menu-item-main menu-item-main-light menu-item-active',
+          contactClass: 'menu-item menu-item-main menu-item-main-light',
+        }));
+
+      ~this.props.currentPage.indexOf('Index') &&
+        (this.setState({
+          menuType: 'dark',
+          ethicodeClass: 'menu-item menu-item-main menu-item-first menu-item-active',
+          aboutClass: 'menu-item menu-item-main',
+          workClass: 'menu-item menu-item-main',
+          contactClass: 'menu-item menu-item-main',
+        }));
     }
   }
   render() {
     return (
-      <header id="home">
-        {this.state.menuType === "dark" ? (
+      this.state.menuType === 'dark' ? (
           <div className="menu menu-main">
             <Link href="/">
-              <a className="router-link-exact-active router-link-active menu-item menu-item-main menu-item-active menu-item-first">
+              <a className={this.state.ethicodeClass}>
                 <img src="/assets/ethicode-logo-white.png" alt="ethicode-logo" className="logo" />
               </a>
             </Link>
             <Link href="/about">
-              <a className="menu-item menu-item-main">About&nbsp;</a>
+              <a className={this.state.aboutClass}>About&nbsp;</a>
             </Link>
             <Link href="/work">
-              <a className="menu-item menu-item-main">Work&nbsp;</a>
+              <a className={this.state.workClass}>Work&nbsp;</a>
             </Link>
             <Link href="/contact">
-              <a className="menu-item menu-item-main">Contact&nbsp;</a>
+              <a className={this.state.contactClass}>Contact&nbsp;</a>
             </Link>
           </div>
         ) : (
           <div className="menu menu-main menu-main-light">
             <Link href="/">
-              <a className="router-link-exact-active router-link-active menu-item menu-item-main menu-item-active menu-item-first">
+              <a className="menu-item menu-item-main menu-item-active menu-item-first">
                 <img src="/assets/ethicode-logo-black.png" alt="ethicode-logo" className="logo" />
               </a>
             </Link>
             <Link href="/about">
-              <a className="menu-item menu-item-main menu-item-main-light">About&nbsp;</a>
+              <a className={this.state.aboutClass}>About&nbsp;</a>
             </Link>
             <Link href="/work">
-              <a className="menu-item menu-item-main menu-item-main-light">Work&nbsp;</a>
+              <a className={this.state.workClass}>Work&nbsp;</a>
             </Link>
             <Link href="/contact">
-              <a className="menu-item menu-item-main menu-item-main-light">Contact&nbsp;</a>
+              <a className={this.state.contactClass}>Contact&nbsp;</a>
             </Link>
           </div>
-        )}
-      </header>
+        )
     );
   }
 }
